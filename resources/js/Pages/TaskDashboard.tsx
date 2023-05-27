76eaccfcb7fc1af9dc652e8c316fcf91ed0b50ab
Nathan <nslee333@gmail.com>
2023-05-26 19:03:06 -0700

    React frame for displaying the tasks

--- resources/js/Pages/TaskDashboard.tsx
+++ resources/js/Pages/TaskDashboard.tsx
@@ -1,10 +1,36 @@
 import { Link, Head } from '@inertiajs/react';
 
+
+function displayTasks() {
+
+
+  return (
+    <>
+      <div>
+        
+      </div>
+    </>
+  );
+}
+
+
+
+
 export default function TaskDashboard() {
   return (
     <>
-      <div className="bg-[#3f3f3f] h-screen">
+      <div className="bg-[#3f3f3f] h-screen text-white">
           <Head title="Task Dashboard" />
+          <div className="bg-[#33333] h-[8rem] w-screen ">
+            <Link href="/" className="">Logout</Link>
+          </div>
+
+          <div className=" flex justify-center">
+            <div className="bg-[#222] h-[80vh] w-1/2 rounded-md p-5">
+              
+            </div>
+          </div>
+
 
       </div>
     </>
