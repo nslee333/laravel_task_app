c547f881a93957112e445c4ef8ebe2426ea19549
Nathan <nslee333@gmail.com>
2023-05-30 21:25:58 -0700

    Extracted mock data into seperate file

---
+++ resources/js/Pages/data.ts
@@ -0,0 +1,115 @@
+
+
+
+
+export const data = [
+    {
+      id: 0,
+      task: "do laundry"
+    },
+    {
+      id: 1,
+      task: "do laundry"
+    },
+    {
+      id: 2,
+      task: "do laundry"
+    },
+    {
+      id: 3,
+      task: "do laundry"
+    },
+    {
+      id: 4,
+      task: "do laundry"
+    },
+    {
+      id: 5,
+      task: "do laundry"
+    },
+    {
+      id: 3,
+      task: "do laundry"
+    },
+    {
+      id: 4,
+      task: "do laundry"
+    },
+    {
+      id: 5,
+      task: "do laundry"
+    },
+    {
+      id: 3,
+      task: "do laundry"
+    },
+    {
+      id: 4,
+      task: "do laundry"
+    },
+    {
+      id: 5,
+      task: "do laundry"
+    },
+    {
+      id: 3,
+      task: "do laundry"
+    },
+    {
+      id: 4,
+      task: "do laundry"
+    },
+    {
+      id: 5,
+      task: "do laundry"
+    },
+    {
+      id: 3,
+      task: "do laundry"
+    },
+    {
+      id: 4,
+      task: "do laundry"
+    },
+    {
+      id: 5,
+      task: "do laundry"
+    },
+    {
+      id: 3,
+      task: "do laundry"
+    },
+    {
+      id: 4,
+      task: "do laundry"
+    },
+    {
+      id: 5,
+      task: "do laundry"
+    },
+    {
+      id: 3,
+      task: "do laundry"
+    },
+    {
+      id: 4,
+      task: "do laundry"
+    },
+    {
+      id: 5,
+      task: "do laundry"
+    },
+    {
+      id: 3,
+      task: "do laundry"
+    },
+    {
+      id: 4,
+      task: "do laundry"
+    },
+    {
+      id: 5,
+      task: "do laundry"
+    },
+  ]
+  
