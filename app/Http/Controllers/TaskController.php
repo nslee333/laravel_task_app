0ea47b8a112e250b3465e038fb61a1e7895d7ca0
Nathan <nslee333@gmail.com>
2023-06-01 10:17:34 -0700

    Init task controller

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class TaskController extends Controller
+{
+    function get_tasks()
+    {
+
+    }
+
+    function add_task()
+    {
+
+    }
+
+    function delete_task()
+    {
+
+    }
+}
