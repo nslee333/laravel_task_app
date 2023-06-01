a262eb9bbdba27a62b77c7dc7e60c72d5722b2c5
Nathan <nslee333@gmail.com>
2023-06-01 10:28:07 -0700

    Added pseudocode for controller

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -9,16 +9,39 @@ class TaskController extends Controller
 {
     function get_tasks()
     {
+        // & Grab the current user.
+        $user = User::find();
+
+        // & $tasks = $user->tasks;
+
+        // & return tasks to caller.
+    }
+
+    function add_task() // * Add param for new task
+    {
+        // & grab the current user
+        // & grab their tasks.
+
+        // & json_decode the json.
+        // & push param task to array.
+        // & json_encode the array back to json.
+        // & save to database.
+
+        // & Return success or failure.
+
 
     }
 
-    function add_task()
+    function delete_task() // * index param -> task to be deleted.
     {
+        // & grab the current user.
+        // & grab their tasks.
 
-    }
+        // & json_decode the json.
+        // & delete the task from the tasks.
+        // & json_encode the array.
+        // & save to the database.
 
-    function delete_task()
-    {
-
+        // & return sucess or failure.
     }
 }
