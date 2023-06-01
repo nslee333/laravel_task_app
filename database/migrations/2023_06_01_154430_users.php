f908f7b02c064f70089a51f0cf9c5ec7abea96cd
Nathan <nslee333@gmail.com>
2023-06-01 09:54:26 -0700

    Added migration to add tasks column to Users table

--- database/migrations/2023_06_01_154430_users.php
+++ database/migrations/2023_06_01_154430_users.php
@@ -11,7 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::table('users', function(Blueprint $table) {
+            $data = json_encode(array("Mow the lawn"));
+            $table->json('tasks')->default($data);
+        });
     }
 
     /**
@@ -19,6 +22,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::table('users', function(Blueprint $table) {
+            $table->dropColumn('tasks');
+        });
     }
 };
