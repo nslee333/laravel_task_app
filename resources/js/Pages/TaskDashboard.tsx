f6ab46b6be48a55da613f4f3c899746e478d59ce
Nathan <nslee333@gmail.com>
2023-05-31 18:35:29 -0700

    Formatting

--- resources/js/Pages/TaskDashboard.tsx
+++ resources/js/Pages/TaskDashboard.tsx
@@ -1,144 +1,137 @@
-import { Link, Head } from '@inertiajs/react';
-import { Fragment, SyntheticEvent } from 'react';
-import { useState} from 'react';
-import {data} from "./data";
+import { Link, Head } from "@inertiajs/react";
+import { Fragment, SyntheticEvent } from "react";
+import { useState } from "react";
+import { data } from "./data";
 
+function DisplayTasks({ tasks, deleteTask }: { tasks: any; deleteTask: any }) {
+    const displayTasks = tasks.map((element, index) => {
+        if (!tasks) return;
 
-
-function DisplayTasks({tasks, deleteTask}: {tasks: any, deleteTask: any}) {   
-
-  const displayTasks = tasks.map((element, index) => {
-    if (!tasks) return;
+        return (
+            <Fragment key={index}>
+                <div className="flex justify-between sm:text-3xl xs:text-sm">
+                    <div>{tasks[index].task}</div>
+                    <button onClick={(e) => deleteTask(e, index)}>
+                        &times;
+                    </button>
+                </div>
+            </Fragment>
+        );
+    });
 
     return (
-      <Fragment key={index}>
-        <div className="flex justify-between sm:text-3xl xs:text-sm">
-          <div>{tasks[index].task}</div>
-          <button onClick={e => deleteTask(e, index)}>&times;</button>
-        </div>
-      </Fragment>
-    )
-  });
-  
-  
-  return (
-    <>
-      <div>
-        {displayTasks}
-      </div>
-    </>
-  );
+        <>
+            <div>{displayTasks}</div>
+        </>
+    );
 }
 
+function Input(props: { addTask: any; draft: any; setDraft }) {
+    const { draft, setDraft } = props;
 
-function Input(props: {addTask: any, draft: any, setDraft}) {  
-  const {draft, setDraft} = props;
-  
-  function handleSubmit(e: any) {
-    const {addTask} = props;
-    e.preventDefault();
-  
-    const target = e.target[0];
-  
-    if (target) {
-      addTask(target.value);
+    function handleSubmit(e: any) {
+        const { addTask } = props;
+        e.preventDefault();
+
+        const target = e.target[0];
+
+        if (target) {
+            addTask(target.value);
+        }
     }
-    
-  }
-  
-  return (
-    <div className="bg">
-      <form onSubmit={e => handleSubmit(e)}>
 
-        <input className="bg-[#333333] 
-                w-[100%] 
-                text-2xl 
-                rounded-md 
-                mb-5
+    return (
+        <div className="bg">
+            <form onSubmit={(e) => handleSubmit(e)}>
+                <input
+                    className="bg-[#333333] 
+                                w-[100%] 
+                                text-2xl 
+                                rounded-md 
+                                mb-5
                 
-                " 
-                id="task" 
-                type="text" 
-                placeholder="get bailey leaves..."
-                onChange={e => setDraft(e.target.value)}
-                value={draft}
-        />
-      </form>
-    </div>
-  );
-};
-
-
+                "
+                    id="task"
+                    type="text"
+                    placeholder="get bailey leaves..."
+                    onChange={(e) => setDraft(e.target.value)}
+                    value={draft}
+                />
+            </form>
+        </div>
+    );
+}
 
 export default function TaskDashboard() {
-  const [tasks, setTasks] = useState(data);
-  const [draft, setDraft] = useState("");
+    const [tasks, setTasks] = useState(data);
+    const [draft, setDraft] = useState("");
 
-  function extractTasks(): any[] {
-    let newArr = [];
-    
-    if (!tasks) return [];
+    function extractTasks(): any[] {
+        let newArr = [];
 
-    tasks.forEach(element => {
-      newArr.push(element);
-    });
-    
-    return newArr;
-  }
-  
-  
-  function deleteTask(e: SyntheticEvent, index: number) {
-    e.preventDefault();
-        
-    const newArr = extractTasks();
-    
-    newArr.splice(index, 1);
-    
-    setTasks(newArr);
-  }
+        if (!tasks) return [];
 
-  function addTask(draftParam: string) {
-    if (draftParam.length === 0) return;
+        tasks.forEach((element) => {
+            newArr.push(element);
+        });
 
-    setDraft(draftParam);
-
-    const newArr = extractTasks();
-
-    if (tasks && tasks.length > 0) {
-      newArr.push({
-        id: tasks[tasks.length - 1].id + 1,
-        task: draft
-      });
-
-    } else {
-      newArr.push({
-        id: 0,
-        task: draft
-      });
-      
+        return newArr;
     }
-    
-    setTasks(newArr);
-    setDraft("")
-  }
 
+    function deleteTask(e: SyntheticEvent, index: number) {
+        e.preventDefault();
 
-  return (
-    <div>
-      <div className="bg-[#3f3f3f] min-h-[100vh] text-white">
-          <Head title="Task Dashboard" />
-          <div className="h-[8rem] w-screen ">
-            <Link href="/" className="">Logout</Link>
-          </div>
+        const newArr = extractTasks();
 
-          <div className=" flex justify-center">
-            <div className="bg-[#222] h-[80vh] overflow-auto sm:w-1/2 xs:w-7/8 rounded-md p-5 mb-10 border-2 border-pink-500">
-              <Input addTask={addTask} draft={draft} setDraft={setDraft}/>
-              <DisplayTasks tasks={tasks} deleteTask={deleteTask} />
+        newArr.splice(index, 1);
+
+        setTasks(newArr);
+    }
+
+    function addTask(draftParam: string) {
+        if (draftParam.length === 0) return;
+
+        setDraft(draftParam);
+
+        const newArr = extractTasks();
+
+        if (tasks && tasks.length > 0) {
+            newArr.push({
+                id: tasks[tasks.length - 1].id + 1,
+                task: draft,
+            });
+        } else {
+            newArr.push({
+                id: 0,
+                task: draft,
+            });
+        }
+
+        setTasks(newArr);
+        setDraft("");
+    }
+
+    return (
+        <div>
+            <div className="bg-[#3f3f3f] min-h-[100vh] text-white">
+                <Head title="Task Dashboard" />
+                <div className="h-[8rem] w-screen ">
+                    <Link href="/" className="">
+                        Logout
+                    </Link>
+                </div>
+
+                <div className=" flex justify-center">
+                    <div className="bg-[#222] h-[80vh] overflow-auto sm:w-1/2 xs:w-7/8 rounded-md p-5 mb-10 border-2 border-pink-500">
+                        <Input
+                            addTask={addTask}
+                            draft={draft}
+                            setDraft={setDraft}
+                        />
+                        <DisplayTasks tasks={tasks} deleteTask={deleteTask} />
+                    </div>
+                </div>
             </div>
-          </div>
-
-      </div>
-    </div>
-  );
+        </div>
+    );
 }
