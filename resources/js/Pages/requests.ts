729c490f9d92b5c22f02051368f553615b3caada
Nathan <nslee333@gmail.com>
2023-06-01 20:01:39 -0700

    Filled in addTask and deleteTask methods

--- resources/js/Pages/requests.ts
+++ resources/js/Pages/requests.ts
@@ -14,14 +14,34 @@ function getTasks() {
   return getResponse;
 }
 
-function addTask() {
+function addTask(newTask: string) {
 
-  const
+  const addResponse = axios.post("/tasks", {
+    new_task: newTask
+  })
+    .then(function (response) {
+      return response;
+    })
+    .catch(function (error)  {
+      return Error(error);
+    });
 
+    return addResponse;
 }
 
-function deleteTask() {
+function deleteTask(taskIndex: number) {
 
+  const deleteResponse = axios.delete("tasks", { data: {
+      task_index: taskIndex
+    }})
+    .then(function (response) {
+      return response;
+    })
+    .catch(function (error) {
+      return Error(error);
+    });
+
+  return deleteResponse;
 }
 
 export { getTasks, addTask, deleteTask };
