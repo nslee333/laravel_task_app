bdc54360e9273d30fd68fc0f71d1855555270e59
Nathan <nslee333@gmail.com>
2023-06-02 17:09:45 -0700

    Formatting

--- resources/js/Pages/requests.ts
+++ resources/js/Pages/requests.ts
@@ -1,48 +1,49 @@
 import axios from "axios";
 import { AxiosResponse, AxiosError } from "axios";
 
-
 async function getTasksReq() {
+    const getResponse = await axios
+        .get("/tasks")
+        .then(function (response) {
+            return response;
+        })
+        .catch(function (error) {
+            return error;
+        });
 
-  const getResponse = await axios.get('/tasks')
-    .then(function(response) {
-      return response;
-    })
-    .catch(function (error) {
-      return error;
-    });
-
-  return getResponse;
+    return getResponse;
 }
 
 async function addTaskReq(newTask: string) {
-
-  const addResponse = await axios.post("/tasks", {
-    new_task: newTask
-  })
-    .then(function (response) {
-      return response;
-    })
-    .catch(function (error)  {
-      return error;
-    });
+    const addResponse = await axios
+        .post("/tasks", {
+            new_task: newTask,
+        })
+        .then(function (response) {
+            return response;
+        })
+        .catch(function (error) {
+            return error;
+        });
 
     return addResponse;
 }
 
 async function deleteTaskReq(taskIndex: number) {
+    const deleteResponse = await axios
+        .delete("tasks", {
+            data: {
+                task_index: taskIndex,
+            },
+        })
+        .then(function (response) {
+            return response;
+        })
+        .catch(function (error) {
+            return error;
+        });
 
-  const deleteResponse = await axios.delete("tasks", { data: {
-      task_index: taskIndex
-    }})
-    .then(function (response) {
-      return response;
-    })
-    .catch(function (error) {
-      return error;
-    });
-
-  return deleteResponse;
+    return deleteResponse;
 }
 
 export { getTasksReq, addTaskReq, deleteTaskReq };
